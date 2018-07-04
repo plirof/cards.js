84d24eee2c08be19f3c7123b09b16ee7536e97ab
aaa <aaa@aaa.com>
2018-07-04 12:40:14 +0300

    example jon 01 v003

--- example_jon01.js
+++ example_jon01.js
@@ -1,4 +1,6 @@
 
+$clicked_cards=1;
+
 //Tell the library which element to use for the table
 cards.init({table:'#card-table'});
 
@@ -23,11 +25,14 @@ discardPile = new cards.Deck({faceUp:true});
 discardPile.x += 50;
 
 pl1_slot1 = new cards.Deck({faceUp:true});
-pl1_slot1.y+=100;
-pl1_slot1.x+=60;
+pl1_slot1.y+=240;
+pl1_slot1.x+=0;
 pl1_slot2 = new cards.Deck({faceUp:true});
+pl1_slot2.y+=240;
+pl1_slot2.x+=80;
 pl1_slot3 = new cards.Deck({faceUp:true});
-
+pl1_slot3.y+=240;
+pl1_slot3.x+=160;
 
 //Let's deal when the Deal button is pressed:
 $('#deal').click(function() {
@@ -56,8 +61,23 @@ deck.click(function(card){
 //the same suit or rank as the top card of the discard pile
 //then it's added to it
 lowerhand.click(function(card){
-	pl1_slot1.addCard(card);
-	pl1_slot1.render();
+	
+	if($clicked_cards==1) {
+		pl1_slot1.addCard(card);
+		pl1_slot1.render();
+		$clicked_cards++;
+	}else
+	if($clicked_cards==2) {
+		pl1_slot2.addCard(card);
+		pl1_slot2.render();
+		$clicked_cards++;
+	}else	
+	if($clicked_cards==3) {
+		pl1_slot3.addCard(card);
+		pl1_slot3.render();
+		$clicked_cards=1;
+	}else {$clicked_cards=1;}	
+/*
 	if (card.suit == discardPile.topCard().suit 
 		|| card.rank == discardPile.topCard().rank) {
 
@@ -65,6 +85,7 @@ lowerhand.click(function(card){
 		discardPile.render();
 		lowerhand.render();
 	}
+	*/
 });
 
 
